c8811d784edf35d45b6b909ad099b007000c8d78
alexey.lysiuk <alexey.lysiuk@gmail.com>
2025-04-29 18:20:01 +0300

    no message

--- target/main.py
+++ target/main.py
@@ -15,3 +15,23 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+
+from aedi.state import BuildState
+from aedi.target.base import CMakeMainTarget
+
+
+class SdrPlusPlusTarget(CMakeMainTarget):
+    def __init__(self, name='sdrpp'):
+        super().__init__(name)
+
+    def prepare_source(self, state: BuildState):
+        state.checkout_git('https://github.com/AlexandreRouma/SDRPlusPlus.git')
+
+    def configure(self, state: BuildState):
+        opts = state.options
+        opts['OPT_BUILD_AIRSPY_SOURCE'] = 'NO'
+        opts['OPT_BUILD_AIRSPYHF_SOURCE'] = 'NO'
+        opts['OPT_BUILD_AUDIO_SOURCE'] = 'NO'
+        opts['OPT_BUILD_AUDIO_SINK'] = 'NO'
+
+        super().configure(state)
