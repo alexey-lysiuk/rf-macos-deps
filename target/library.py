4eea08a39a116cbdec899e82e90c2c3f496082b5
alexey.lysiuk <alexey.lysiuk@gmail.com>
2025-05-10 13:22:56 +0300

    no message

--- target/library.py
+++ target/library.py
@@ -122,7 +122,9 @@ class FobosTarget(base.CMakeSharedDependencyTarget):
     def post_build(self, state: BuildState):
         super().post_build(state)
 
-        if not state.xcode:
+        if state.xcode:
+            self.link_xcode_deps(state, 'usb')
+        else:
             for suffix in ('devinfo', 'fwloader', 'recorder'):
                 self.copy_to_bin(state, 'fobos_' + suffix)
 
@@ -133,15 +135,18 @@ class FobosAgileTarget(base.CMakeSharedDependencyTarget):
         self.project_name = 'fobos_sdr'
 
     def prepare_source(self, state: BuildState):
+        patches = ['fobos-agile-fix-' + suffix for suffix in ('cmake', 'determinism', 'open', 'pc', 'tools')]
         state.download_source(
             'https://github.com/rigexpert/libfobos-sdr-agile/archive/refs/tags/v.3.0.2.tar.gz',
-            'fa41189cc23b718f73386d05f9b0809127cd21abafc74dd60af600c79e08247a')
-            # TODO: patches=('fobos-fix-cmake', 'fobos-fix-determinism', 'fobos-fix-open'))
+            'fa41189cc23b718f73386d05f9b0809127cd21abafc74dd60af600c79e08247a',
+            patches)
 
     def post_build(self, state: BuildState):
         super().post_build(state)
 
-        if not state.xcode:
+        if state.xcode:
+            self.link_xcode_deps(state, 'usb')
+        else:
             for suffix in ('devinfo', 'fwloader', 'recorder', 'scanner'):
                 self.copy_to_bin(state, 'fobos_sdr_' + suffix)
 
